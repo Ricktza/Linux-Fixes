1a4fb566f1a00b6930fde789537e9928d1ddc17d
Ricktza <Ricktza@users.noreply.github.com>
2025-12-31 15:45:22 +0000

    Add comments for Steam startup and joystick settings

--- MSC-MWC.sh
+++ MSC-MWC.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # My Winter Car - G29 Axis Correction Script
 
+# Steam startup params
+# ~/scripts/MSC-MWC.sh && game-performance %command%
 DEVICE="/dev/input/by-id/usb-Logitech_G29_Driving_Force_Racing_Wheel-event-joystick"
 
 # Steering (Symmetric range for 1:1 feel)
 evdev-joystick --e "$DEVICE" -a 0 -m -65535 -M 65535 --deadzone 0
 
 # Pedals (Fixes the 30% "wonky" reset issue)
+# Ingame, normalisation enabled + invert.
 evdev-joystick --e "$DEVICE" -a 1 -m -255 -M 255 --deadzone 0 
 evdev-joystick --e "$DEVICE" -a 2 -m -255 -M 255 --deadzone 0 
 evdev-joystick --e "$DEVICE" -a 5 -m -255 -M 255 --deadzone 0
